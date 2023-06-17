fb7ff940c9bc82ba13e29e437870aa81250e7f10
D K <s23109@pjwstk.edu.pl>
2023-06-17 16:44:27 +0200

    Models : Part, ProductReference, Sale

---
+++ MAS_PROJ/Shared/Models/Part.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS_PROJ.Shared.Models
+{
+    public class Part
+    {
+        public Part()
+        {
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string PartType { get; set; }
+        public string PartCategory { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal CurrentPrice { get; set; }
+
+        public static DateOnly OldestReturnDate { get; set; }
+    }
+}
---
+++ MAS_PROJ/Shared/Models/ProductReference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS_PROJ.Shared.Models
+{
+    public class ProductReference
+    {
+        public ProductReference()
+        {
+        }
+        public decimal? SalePrice { get; set; }
+    }
+}
