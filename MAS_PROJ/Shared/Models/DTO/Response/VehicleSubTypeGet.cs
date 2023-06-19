977f4d7d3018674ca1ecbff0db2ea9690d57447d
D K <s23109@pjwstk.edu.pl>
2023-06-19 17:15:57 +0200

    Start VehicleDetails component + appropiate DTO-s

---
+++ MAS_PROJ/Shared/Models/DTO/Response/VehicleSubTypeGet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS_PROJ.Shared.Models.DTO.Response
+{
+    public class VehicleSubTypeGet
+    {
+        public int IdSubtype { get; set; }
+        public string Name { get; set; }
+        public string? SubtypeNotes { get; set; }
+        public SubType SubType { get; set; }
+
+        //Complex Attributes
+        public FuelSpecifics? FuelSpecifics { get; set; }
+        public PoiseSpecifics? PoiseSpecifics { get; set; }
+        public PurposeSpecifics? PurposeSpecifics { get; set; }
+    }
+
+    public enum SubType { Land, Water }
+}
